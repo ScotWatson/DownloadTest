5cbdff43bdbd2d7fe3b1577a825c0536c1410b13
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-02-06 17:52:28 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,11 +3,13 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-let contents = new Blob( [ "Hello World!" ] );
-let a = document.createElement("a");
-a.href = URL.createObjectURL(contents);
-a.download = "hello.txt";
-document.body.appendChild(a);
-a.click();
-a.remove();
-URL.revokeObjectURL(a.href);
+window.addEnetListener("click", function () {
+  let contents = new Blob( [ "Hello World!" ] );
+  let a = document.createElement("a");
+  a.href = URL.createObjectURL(contents);
+  a.download = "hello.txt";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(a.href);
+});
